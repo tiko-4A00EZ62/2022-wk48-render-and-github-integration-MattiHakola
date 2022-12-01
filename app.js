4858cb710726f569624d36962653642a03f37d85
Matti Hakola <matti.hakola99@gmail.com>
2022-12-01 09:58:50 +0200

    app

--- app.js
+++ app.js
@@ -4,4 +4,9 @@ const electricityRouter = require("./routes/electricity");
 const app = express();
 app.use(express.json());
 app.use("/api/electricity", electricityRouter);
+
+app.get("/health", (req, res) => {
+    res.send("OK");
+});
+
 module.exports = app;
